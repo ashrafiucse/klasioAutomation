745a952204a7e72f593d0db1d33bbc3cdfd0a06e
Ashraf Ali <ashraf.iu.cse1718@gmail.com>
2024-11-25 00:40:41 +0600

    Clickable,visible,dropdown,getElementText custom method added

--- src/test/java/pages/AdminRegistrationPage.java
+++ src/test/java/pages/AdminRegistrationPage.java
@@ -1,5 +1,27 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class AdminRegistrationPage extends BasePage{
     public String adminRegistrationPageUrl = "https://app.klasio.dev/registration";
+    public By firstName = By.id("form.first_name");
+    public By lastName = By.id("form.last_name");
+    public By emailField = By.id("form.email");
+    public By passwordField = By.id("form.password");
+    public By repeatPasswordField = By.id("form.password_confirmation");
+    public By termsAndConditionCheckBox = By.id("form.terms");
+    public By ContinueButton = By.xpath("//button[@type='submit']");
+    public By logInLink = By.linkText("//button[@type='submit']");
+    public String emailAlreadyExistWarningMsg = "Email address already exists.";
+    public String lastNameRequiredWarningMsg = "Last name is required.";
+    public String passwordNotMatchWarningMsg = "Password does not match.";
+    public String termAndConditionNotAcceptWarningMsg = "You must agree to the terms and conditions.";
+    public String emailFieldRequiredMsg = "The form.email field is required.";
+
+    public By lastNameRequireWarningElement = By.xpath("//p[contains(text(),'Last name is required.')]");
+    public By emailAlreadyExistWarningElement = By.xpath("//p[contains(text(),'Email address already exists.')]");
+    public By passwordNotMatchWarningElement = By.xpath("//p[contains(text(),'Password does not match.')]");
+    public By termAndConditionNotAcceptWarningElement = By.xpath("//p[contains(text(),'You must agree to the terms and conditions.')]");
+    public By emailFieldRequiredElement = By.xpath("//p[contains(text(),'The form.email field is required.')]");
+
 }
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,17 +2,46 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static utilities.DriverSetup.getDriver;
 
 public class BasePage {
     public WebElement getElement(By locator) {
+        waitForElementToBeVisible(locator);
         return getDriver().findElement(locator);
     }
     public void clickOnElement(By locator) {
+        waitForElementToBeClickable(locator);
         getElement(locator).click();
     }
+
+    public boolean isElementVisible(By locator) {
+        return getElement(locator).isDisplayed();
+    }
+
     public void sendKeysText(By locator,String inputText) {
+        getElement(locator).clear();
         getElement(locator).sendKeys(inputText);
+    }
+    public void waitForElementToBeVisible(By locator) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+    public void waitForElementToBeClickable(By locator) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+    public void dropDownOptionSelectByIndex(By locator, String optionText) {
+        Select select = new Select(getElement(locator));
+        select.selectByVisibleText(optionText);
+    }
+    public String getElementText(By locator) {
+        return getElement(locator).getText();
     }
 }
